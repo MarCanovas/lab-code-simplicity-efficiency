caedcad6dd66bd5dd054adfee47c25646218734c
MarCanovas <canovasgarciamar@gmail.com>
2020-07-05 22:26:44 +0200

    Challenge 2 done

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -6,29 +6,22 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+import random
+import string
+import sys
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+
+def RandomStringGenerator(stringLength=12):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
 
 def BatchStringGenerator(n, a=8, b=12):
-    r = []
+    #Array of random strings
+    r = []          
+
     for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
+        r.append(RandomStringGenerator(random.choice(range(a, b))))
     return r
 
 a = input('Enter minimum string length: ')
